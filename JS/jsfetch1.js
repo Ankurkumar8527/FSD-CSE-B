6f4e4ddd3cd465fc350757400a75b694cc29a0db
Ankur Kumar <ankursonisharp@gmail.com>
2025-09-25 14:58:28 +0530

    dom

--- JS/jsfetch1.js
+++ JS/jsfetch1.js
@@ -1,20 +1,41 @@
 const url=("https://dummyjson.com/users?limit=2");
-const pr=fetch(url);
-pr.then((res)=>{
-    return res.json();
-})
-// bcoz users is an error
-.then((data)=>
-{   data.users.forEach((da)=>
-{
-     console.log("name=",da.firstName);
-    console.log("name=",da.email);
-    console.log("name=",da.ip);
-    console.log("name=",da.macAddress);
-})
+// const pr=fetch(url);
+// pr.then((res)=>{
+//     return res.json();
+// })
+// // bcoz users is an error
+// .then((data)=>
+// {   data.users.forEach((da)=>
+// {
+//     console.log("name=",da.firstName);
+//     console.log("name=",da.email);
+//     console.log("name=",da.ip);
+//     console.log("name=",da.macAddress);
+// })
    
-})
-.catch((err)=>
+// })
+// .catch((err)=>
+// {
+//     console.log("Error: ",err.message);
+// })
+
+const f1=async()=>
 {
-    console.log("Error: ",err.message);
-})
+   try{
+    const res=await fetch(url);
+    const jsondata=await res.json();
+    jsondata.users.forEach((data)=>
+    {
+        console.log("name=",data.firstName);
+        console.log("Email=",data.email);
+        console.log("IP=",data.ip);
+        console.log("Mac=",data.macAddress);
+        console.log("##########");
+    })
+   }
+   catch(err)
+   {
+    console.log("error= ",err.message);
+   }
+}
+f1();
